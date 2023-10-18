bd37b8e8f4adb04dacac3db0ee71ac7fba34db1f
ween <228574227@qq.com>
2023-10-18 11:06:30 +0800

    自动部署相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,18 @@ import { defineConfig } from 'vite'
 import { createVitePlugins } from './config/vitePlugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+export default defineConfig(({ mode }) => {
+  const base = mode === 'development' ? '/' : '/WebglStudy/'
+  return {
+    base,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    plugins: createVitePlugins(),
+    esbuild: {
+      pure: ['console.log'] // 去除console.log
     }
-  },
-  plugins: createVitePlugins()
+  }
 })
